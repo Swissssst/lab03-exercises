79d4f1c7619921aa67a4e9f4948fd7e0120149c7
Swissssst <jimbobhe100th@gmail.com>
2025-02-05 16:11:31 -0500

    find_duplicates.py test

---
+++ python/find_duplicates.py
@@ -0,0 +1,20 @@
+def find_duplicates_nested_loop(l: list) -> list:
+    seen = set()
+    duplicates = set()
+
+    for num in l:
+        if num in seen:
+            duplicates.add(num)
+        seen.add(num)
+    return list(duplicates)
+
+if __name__ == "__main__":
+    sample1 = [3, 7, 5, 6, 7, 4, 8, 5, 7, 66]
+    sample2 = [3, 5, 6, 4, 4, 5, 66, 6, 7, 6]
+    sample3 = [3, 0, 5, 1, 0]
+    sample4 = [3]
+    
+    print("Sample 1:", find_duplicates_nested_loop(sample1))
+    print("Sample 2:", find_duplicates_nested_loop(sample2))
+    print("Sample 3:", find_duplicates_nested_loop(sample3))
+    print("Sample 4:", find_duplicates_nested_loop(sample4))
